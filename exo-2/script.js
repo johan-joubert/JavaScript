73ee5e141e98e27d9bab0c15b243b2174d9e69c9
Johan <njoubert06@gmail.com>
2021-01-07 16:49:48 +0100

    exo 3 done

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,21 @@
 // you can write js here
-console.log('exo-2');
+
+let isRegistered = false;
+
+let ageRunner = 7;
+
+
+if (!isRegistered) {
+    var raceNumber = Math.floor(Math.random()*1000);
+}
+
+if (ageRunner > 18 && isRegistered) {
+    console.log('You will race at 9:30 am');
+} else if (isRegistered || ageRunner > 18) {
+    console.log('You will race at 11:00 am');
+} else if (ageRunner < 18 && ageRunner > 8 ) {
+    console.log('You will race at 12:30 pm ' + raceNumber);
+} else {
+    console.log('Go to see the registration desk');
+}
+
--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -1,2 +1,76 @@
 // you can write js here
-console.log('exo-3');
+function getUserChoice(userInput) {
+    userInput = userInput.toLowerCase();
+
+    if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'britney') {
+        return userInput;
+    } else {
+        return console.log('please enter a correct choice');
+    }
+};
+
+function getComputerChoice() {
+   let RandomNumber = Math.floor(Math.random() * 3);
+   let choiceComputer;
+    switch(RandomNumber) {
+        case 0:
+            return choiceComputer = 'rock';
+            break;
+        case 1:
+            return choiceComputer = 'paper';
+            break;
+        case 2:
+            return choiceComputer = 'scissors';
+            break;
+    }
+    return choiceComputer;
+}
+
+function determineWinner(userChoice, computerChoice) {
+    if(userChoice === computerChoice) {
+        return 'Tied';
+    }
+    else if (userChoice === 'rock') {
+        if (computerChoice === 'scissors') {
+            return 'Won';
+        } else {
+            return 'Lost';
+        }
+    }
+
+    if (userChoice === 'paper') {
+        if (computerChoice === 'rock') {
+            return 'Won';
+        } else {
+            return 'Lost';
+        }
+    }
+
+    if (userChoice === 'scissors') {
+        if (computerChoise === 'paper') {
+            return 'Won';
+        } else {
+            return 'Lost';
+        }
+    }
+
+    if (userChoice === 'britney') {
+        if (computerChoice === 'paper' || computerChoice === 'rock' || computerChoice === 'scissors') {
+            return 'Won';
+        }
+    }
+}
+
+function playGame() {
+    let userChoice =  getUserChoice(prompt('votre choix :'));
+
+    let computerChoice = getComputerChoice();
+
+    console.log('Votre choix : ' + userChoice);
+
+    console.log('Choix de votre adverssaire : ' + computerChoice);
+
+    console.log(determineWinner(userChoice, computerChoice));
+}
+
+playGame();
