2f55ebdfe789d70019fde494e78ea2af9befc692
Johan <njoubert06@gmail.com>
2021-01-07 17:04:38 +0100

    exo 4 done

--- exo-4/script.js
+++ exo-4/script.js
@@ -1,5 +1,17 @@
 // you can write js here
 
-console.log('exo-4');
-
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+secretMessage.pop();
+
+secretMessage.push('to', 'program');
+
+secretMessage[6] = 'right';
+
+secretMessage.shift();
+
+secretMessage.unshift('Programming');
+
+secretMessage.splice(5, 5, 'know');
+
+console.log(secretMessage.join());
