98ea678f782674416129e24c43a3d5a62e714183
Johan <njoubert06@gmail.com>
2021-01-07 14:57:02 +0100

    exo 1 done

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,10 @@
 // you can write js here
-console.log('hello from file');
+
+let kelvin = prompt("Quelle est la température en Kelvin aujourd'hui ?");
+
+let celsius = kelvin - 273; // on convertit le kelvin en celsius
+
+let fahrenheit = celsius * (9/5) + 32; // on convertir en celsius
+
+console.log(Math.floor(fahrenheit)); // j'arrondi le résultat de far
+
