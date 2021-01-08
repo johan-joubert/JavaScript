ef4ecea13dbeafad12441b2e25f3585c13bb0377
Johan <njoubert06@gmail.com>
2021-01-08 10:05:25 +0100

    exo 5 done

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,20 @@
 // you can write js here
 
-console.log('exo-5');
+let input = "manger des bebe phoques";
+
+input = input.toLowerCase();
+
+
+const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+
+let resultArray = [];
+
+
+
+for (let letter of input) {
+    if (vowels.indexOf(letter) !== -1) {
+        resultArray.push(letter);
+    }
+}
+
+console.log(resultArray.join(""));
