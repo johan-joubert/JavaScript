fd48572968b9f1b70c428dc79237670d1804c564
Johan <njoubert06@gmail.com>
2021-01-11 13:32:58 +0100

    exo 7 done

--- exo-7/datas.js
+++ exo-7/datas.js
@@ -1,57 +1,141 @@
-var jsonDatas=[
+var jsonDatas = [
 	{
-		"name" :"Fiat Punto",
-		"type" : "car",
-		"description" : "Je suis une voiture",
-		"price" : 10000,
-		"quantity" : 2
-	},{
-		"name" :"Porsche 911",
-		"type" : "car",
-		"description" : "Je suis une belle voiture",
-		"price" : 80000,
-		"quantity" : 0
-	},{
-		"name" :"Villa sur la plage",
-		"type" : "house",
-		"description" : "Quelle belle vue",
-		"price" : 870000,
-		"quantity" : 1
-	},{
-		"name" :"Peugeot 205",
-		"type" : "car",
-		"description" : "Je suis une autre voiture",
-		"price" : 2000,
-		"quantity" : 2
-	},{
-		"name" :"Maison à la campagne",
-		"type" : "house",
-		"description" : "Vive le calme",
-		"price" : 170000,
-		"quantity" : 3
-	},{
-		"name" :"Monopoly",
-		"type" : "game",
-		"description" : "",
-		"price" : 30,
-		"quantity" : 300
-	},{
-		"name" :"Mario Bros",
-		"type" : "videoGame",
-		"description" : "",
-		"price" : 30,
-		"quantity" : 200
-	},{
-		"name" :"Place VIP concert Metallica",
-		"type" : "show",
-		"description" : "",
-		"price" : 800,
-		"quantity" : 10
-	},{
-		"name" :"Entrée au parc Astérix",
-		"type" : "show",
-		"description" : "",
-		"price" : 30,
-		"quantity" : 200
+		"type": "car",
+		"items": [
+			{
+				"name": "Fiat Punto",
+				"description": "Je suis une voiture",
+				"price": 10000,
+				"quantity": 2,
+				"contact" : {
+					"lastName" 	: "Dubois",
+					"firstName"	: "Martin",
+					"address"	: "1 Grande Rue 74000 Annecy"
+				}
+	
+			},
+			{
+				"name": "Porsche 911",
+				"description": "Je suis une belle voiture",
+				"price": 80000,
+				"quantity": 0,
+				"contact" : {
+					"lastName" 	: "Dufeuille",
+					"firstName"	: "Paul",
+					"address"	: "1 Grande Rue 74000 Annecy"
+				}
+	
+			},
+			{
+				"name": "Peugeot 205",
+				"description": "Je suis une autre voiture",
+				"price": 2000,
+				"quantity": 2,
+				"contact" : {
+					"lastName" 	: "Dugenoux",
+					"firstName"	: "Steven",
+					"address"	: "1 petite Rue 74000 Annecy"
+				}
+	
+			}
+
+		],
+
+	},
+	{
+		"type": "house",
+		"items": [
+			{
+				"name": "Villa sur la plage",
+				"description": "Quelle belle vue",
+				"price": 870000,
+				"quantity": 1,
+				"contact" : {
+					"lastName" 	: "Dupied",
+					"firstName"	: "Martin",
+					"address"	: "1 Grande Rue 74000 Annecy"
+				}
+	
+			},
+			{
+				"name": "Maison à la campagne",
+				"description": "Vive le calme",
+				"price": 170000,
+				"quantity": 0,
+				"contact" : {
+					"lastName" 	: "Ducailloux",
+					"firstName"	: "Martin",
+					"address"	: "1 Grande Rue 74000 Annecy"
+				}
+	
+			}
+		],
+
+	},
+	{
+		"type": "game",
+		"items": [
+			{
+				"name": "Monopoly",
+				"description": "",
+				"price": 30,
+				"quantity": 300,
+				"contact" : {
+					"lastName" 	: "Duchemin",
+					"firstName"	: "Martin",
+					"address"	: "1 Grande Rue 74000 Annecy"
+				}
+	
+			}
+		],
+	},
+	{
+		"type": "videoGame",
+		"items": [
+			{
+				"name": "Mario Bros",
+				"description": "",
+				"price": 30,
+				"quantity": 200,
+				"contact" : {
+					"lastName" 	: "Dumerisier",
+					"firstName"	: "Martin",
+					"address"	: "1 Grande Rue 74000 Annecy"
+				}
+	
+			}
+		],
+	},
+	{
+		"type": "show",
+		"items": [
+			{
+				"name": "Place VIP concert Metallica",
+				"description": "",
+				"price": 800,
+				"quantity": 0,
+				"contact" : {
+					"lastName" 	: "Dubrun",
+					"firstName"	: "Martin",
+					"address"	: "1 Grande Rue 74000 Annecy"
+				}
+	
+			},
+			{
+				"name": "Entrée au parc Astérix",
+				"type": "show",
+				"description": "",
+				"price": 30,
+				"quantity": 200,
+				"contact" : {
+					"lastName" 	: "Dupliquer",
+					"firstName"	: "Martin",
+					"address"	: "1 Grande Rue 74000 Annecy"
+				}
+	
+			}
+		]
 	}
 ]
+
+
--- exo-7/exo7.js
+++ exo-7/exo7.js
@@ -1,3 +1,194 @@
-console.log("exo-7");
+
+
+jsonDatas.forEach(element => {
+    console.log(element);
+})
+
+
+let traduction = {
+    "car": "voiture",
+    "house": "maison",
+    "game": "jeu",
+    "videoGame": "jeux videos",
+    "show": "spectacle"
+}
+
+jsonDatas.forEach(element => {
+    element.translatedType = traduction[element.type];
+})
 
 console.log(jsonDatas);
+
+
+let input = document.createElement('input');
+
+document.body.append(input);
+
+let button = document.createElement('button');
+
+button.textContent = "Afficher type";
+
+document.body.append(button);
+
+let checkbox = document.createElement('input');
+checkbox.setAttribute('type', 'checkbox');
+document.body.append(checkbox);
+
+
+
+let text = document.querySelector('input');
+
+
+button.addEventListener('click', () => {
+
+    for (var i = 0; i < jsonDatas.length; i++) {
+
+        if (jsonDatas[i].translatedType === input.value || jsonDatas[i].type === input.value) {
+            console.log(checkbox.checked);
+
+            if (checkbox.checked) {
+
+                for (var j = 0; j < jsonDatas[i].items.length; j++) {
+                    if (jsonDatas[i].items[j].quantity > 0) {
+                        console.log(jsonDatas[i].items[j]);
+                    }
+                }
+
+            } else {
+
+                jsonDatas[i].items.forEach(article => console.log(article))
+            }
+        }
+    }
+})
+
+let articles= [];
+
+for (var i = 0; i < jsonDatas.length; i++) {
+    for (var j = 0; j < jsonDatas[i].items.length; j++) {
+
+        articles.push(jsonDatas[i].items[j]);
+
+    }
+}
+
+
+let buttonAsc = document.createElement('button');
+buttonAsc.textContent = "Trier par ordre alphabétique";
+document.body.append(buttonAsc);
+
+let buttonDesc = document.createElement('button');
+buttonDesc.textContent = "Trier par ordre désalphabétique";
+document.body.append(buttonDesc);
+
+let buttonPrice = document.createElement('button');
+buttonPrice.textContent = "Trier par prix";
+document.body.append(buttonPrice);
+
+
+buttonAsc.addEventListener('click', () => {
+
+    articles.sort( (a,b) => {
+
+        if(a.name < b.name) {
+            return -1;
+        }
+        if (a.name > b.name) {
+            return 1;
+        }
+        return 0;
+    
+    })
+    
+    console.log(articles);
+  
+})
+
+
+buttonDesc.addEventListener('click', () => {
+
+    articles.sort( (a,b) => {
+
+        if(a.name > b.name) {
+            return -1;
+        }
+        if (a.name < b.name) {
+            return 1;
+        }
+        return 0;
+    
+    })
+    
+    console.log(articles);
+  
+})
+
+buttonPrice.addEventListener('click', () => {
+
+    articles.sort( (a,b) => {
+
+        if(a.price < b.price) {
+            return -1;
+        }
+        if (a.price > b.price) {
+            return 1;
+        }
+        return 0;
+    
+    })
+    
+    console.log(articles);
+  
+})
+
+let type = document.querySelector('#type');
+let name = document.querySelector('#name');
+let description = document.querySelector('#description');
+let price = document.querySelector('#price');
+let quantity = document.querySelector('#quantity');
+let submit = document.querySelector('#submit');
+
+f.addEventListener('submit', () => {
+    
+    let isArticleInserted = false;
+
+    let newArticle = {
+        "name": name.value,
+        "description": description.value,
+        "price": price.value,
+        "quantity": quantity.value 
+    } ;
+    
+    
+    for (let i = 0; i < jsonDatas.length; i++) { 
+        
+            if (type.value == jsonDatas[i].type) {
+    
+                jsonDatas[i].items.push(newArticle);
+
+                isArticleInserted = true;
+    
+            } 
+
+    }
+
+    if(isArticleInserted) {
+        console.log('article inséré avec succès');
+        console.log(jsonDatas);
+    } else {
+        console.log('article non inséré, famille inconnu');
+    }
+
+});
+
+
+for (let i = 0; i < jsonDatas.length; i++) {
+    for (let j = 0; j < jsonDatas[i].items.length; j++) {
+        console.log(jsonDatas[i].items[j].contact);
+    }
+}
+
+
+
+
+
